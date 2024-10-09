c5ef5128daeaadd3ce8aaff18c33a95a0dd03f49
sangminpark9 <52732279+sangminpark9@users.noreply.github.com>
2024-10-09 23:09:03 +0900

    Create sangminpark.py

---
+++ 최단경로/최단경로-1753/sangminpark.py
@@ -0,0 +1,37 @@
+import heapq
+
+V, E = map(int, input().split)
+K = int(input())
+
+graph = [[] for _ in range(V+1)]
+
+for _ in range(E):
+    v,e,w = map(int, input().split())
+    graph[v].append((e,w))
+
+## 입력 끝
+
+def dijkstra(start, V, E):
+    distances = [float('INF')] * (V+1)
+    distances[start] = 0
+    queue = [(0,start)]
+    
+    while queue:
+        current_distance, current_vertex = heapq.heappop(queue)
+        if current_distance > distances[current_vertex]:
+            continue
+        for neighbor, weight in graph[current_vertex]:
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+
+    return distances
+    
+distances = dijkstra(K,V,E)
+
+for i in range(1,len(distances)):
+    if distances[i] == float('INF'):
+        print('INF')
+    else:
+        print(distances[i])
